56136789b0ed54bd1b924946ec4c779813df15a8
Grant Growcott <grantgrowcott56@gmail.com>
2023-06-01 09:45:40 -0400

    react-router-dom addition

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import {useEffect, useState} from'react';
 import { accessToken , logout, getCurrentUserProfile  } from './spotify';
 import { catchErrors } from './utils';
 import './App.css';
+import {
+  BrowserRouter as Router, Routes, Route
+} 
+from'react-router-dom';
 
 
 
@@ -38,19 +42,37 @@ function App() {
           Log in to Spotify 
         </a>
        ) : (
-        <>
-          <h1>Logged in!</h1>
-          <button onClick={logout}>Log out</button>
+        <Router>
+            <Routes>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
 
-          {profile && (
-            <div>
-              <h1>{profile.display_name}</h1>
-              <p>{profile.followers.total} Followers</p>
-              {profile.images.length && profile.images[0].url && (
-                <img src = {profile.images[0].url} alt="Avatar" />)}
-
-            </div>)}
-        </>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar"/>
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Routes>
+          </Router>
        )}
       </header>
     </div>
